6f176ab7602d42b54c352538ee7e8045a5a8b78b
mohamed7ali <mohamed77mosleh77@gmail.com>
2023-03-27 07:36:02 +0200

    K

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,1 @@
-#include <stdio.h> int main(void){puts("\"Programming is like building a multilingual puzzle"); return (0);} 
+#include <stdio.h> int main(void){puts("\"Programming is like building a multilingual puzzle"); return(0);} 
