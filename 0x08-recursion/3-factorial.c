03f1d45df4907cbf98fbba382e5c6b9f62e5220e
mohamed7ali <mohamed77mosleh77@gmail.com>
2024-01-17 23:06:19 +0200

    fact recursion

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -2,18 +2,19 @@
 #include <stdio.h>
 
 /**
- * int factorial(int n) - Returns the factorial of a number.
+ * factorial - Returns the factorial of a number.
  * @n: The number to calculate the factorial for.
  *
  * Return: fact of a num
  */
 int factorial(int n)
 {
-    if (n < 0)
-    {
-        return (-1);
-    }
-/* Base case: fact of 1 is 1 */
+	/* Base case: if n is negative, return -1 */
+	if (n < 0)
+	{
+		return (-1);
+	}
+	/* Base case: factorial of 1 is 1 */
 	if (n == 0)
 	{
 		return (1);
