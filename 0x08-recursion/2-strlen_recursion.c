5f569b4c61ef8fb33386108f0b42c11c477a2505
mohamed7ali <mohamed77mosleh77@gmail.com>
2024-01-17 22:14:35 +0200

    len recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,14 +7,13 @@
  *
  * Return: Length of the string.
  */
+
 int _strlen_recursion(char *s)
 {
-    /* Base case: if the end of the string is reached */
     if (*s == '\0')
     {
         return 0;
     }
 
-    /* Recursive case: return 1 (for the current character) + length of the rest of the string */
     return 1 + _strlen_recursion(s + 1);
 }
