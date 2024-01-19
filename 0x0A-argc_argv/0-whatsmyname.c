2b248296c4853515b2398678f853f106f676ae49
mohamed7ali <mohamed77mosleh77@gmail.com>
2024-01-19 18:51:40 +0200

    argc, argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 
 /**
- * main: is the main function
+ * main - is the main function
  *
  * @argc: The number of parameters including the program name
  *
  * @argv: An array of chars which contains the parameters 
+ *
+ * Return: 0
  */
 
-int main(int argc, char* argv[])
+int main(int argc, char **argv)
 {
-	printf("%s", argv[0]);
+	printf("%s\n", argv[0]);
+
+	return(0);
 }
