ec2f5f976f8e6f027ca558a15c6b841bab078425
mohamed7ali <mohamed77mosleh77@gmail.com>
2024-01-19 18:57:30 +0200

    argc, argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,13 +5,15 @@
  *
  * @argc: The number of parameters including the program name
  *
- * @argv: An array of chars which contains the parameters 
+ * @argv: An array of chars which contains the parameters
  *
  * Return: 0
  */
 
 int main(int argc, char **argv)
 {
+    (void)argc;
+
 	printf("%s\n", argv[0]);
 
 	return(0);
