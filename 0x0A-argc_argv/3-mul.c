0aca6d6223e34498c5691a3f69ebd4cd6034601b
mohamed7ali <mohamed77mosleh77@gmail.com>
2024-01-19 21:15:31 +0200

    argv mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,14 +13,20 @@
 
 int main(int argc, char **argv)
 {
-	if (argc <= 1)
+	if (argc != 3)
 	{
-		printf("Error");
+		printf("Error\n");
 
 		return (1);
 	}
 
-	int result = atoi(argv[1]) * atoi(argv[2]);
 
-	printf("%d\n", result);
+	else
+	{
+		int result = atoi(argv[1]) * atoi(argv[2]);
+
+		printf("%d\n", result);
+	}
+
+	return (0);
 }
