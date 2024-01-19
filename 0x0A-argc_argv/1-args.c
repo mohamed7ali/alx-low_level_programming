28cc446917fa60266d60b60286e3e48646ab8dbb
mohamed7ali <mohamed77mosleh77@gmail.com>
2024-01-19 19:32:04 +0200

    argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+ #include <stdio.h>
+
+/**
+ * main - The entry point of the program
+ *
+ * @argc: The # of arguments
+ *
+ * @argv: Array of chars
+ *
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	printf("%d\n", (argc - 1));
+
+	(void)argv;
+
+	return(0);
+}
