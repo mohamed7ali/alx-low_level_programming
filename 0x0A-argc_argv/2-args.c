7a05071494953e2fb5284121c7f5507f619d1e90
mohamed7ali <mohamed77mosleh77@gmail.com>
2024-01-19 19:59:15 +0200

    argv

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+
+/**
+ * main - The entry point of the program
+ *
+ * @argc: The # of arguments
+ *
+ * @argv: Array of chars
+ *
+ * Return: 0
+ */
+
+
+int main(int argc, char **argv)
+{
+	x = 0;
+
+	while(x != argc)
+	{
+		printf("%s\n", argv[x];
+
+		x += 1
+	}
+
+	return (0);
+}
+
