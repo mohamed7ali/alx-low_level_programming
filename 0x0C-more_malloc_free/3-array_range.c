ca63c08b656e479a6039b2a6bf3df1ad27bc2c82
mohamed7ali <mohamed77mosleh77@gmail.com>
2024-01-20 16:53:53 +0200

    malloc

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,40 +1,19 @@
-#include "main.h"
 #include <stdlib.h>
-
-/**
- * array_range - creates an array of integers
- *
- * @min: The start number of the array
- *
- * @max: The end of the array
- *
- * Return: a pointer to the new allocated space
-*/
-
 
 int *array_range(int min, int max)
 {
-    int x = min;
-    int count, y = 0;
+	int *ptr, x, y = 0;
 
-    if (min > max)
-	    return (NULL);
-
-    for (x; x <= max; x++)
-    {
-        count += 1;
-    }
-
-    int *concat;
-
-    concat = malloc(count * sizeof(int));
-
-    if (concat == NULL)
+	if (min > max)
 		return (NULL);
 
-    for (x = min; x <= max; x++)
-		concat[y++] = x;
+	ptr = malloc((max - min + 1) * sizeof(int));
 
-    return (concat);
+	if (ptr == NULL)
+		return (NULL);
 
+	for (x = min; x <= max; x++)
+		ptr[y++] = x;
+
+	return (ptr);
 }
