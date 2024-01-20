d38b4bca90d1c00b24faa8672a96e381246ba9e5
mohamed7ali <mohamed77mosleh77@gmail.com>
2024-01-20 15:28:52 +0200

    malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -21,8 +21,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *nptr;
 	unsigned int s1len, s2len, x;
 
-	s1len = strlen(s1) + 1;
-	s2len = strlen(s2) + 1;
+	s1len = strlen(s1 + 1);
+	s2len = strlen(s2 + 1);
 
 	if (s2len <= n)
 		n = s2len;
