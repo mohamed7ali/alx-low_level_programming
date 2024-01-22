93cb70fdd4d112bb12f076cb26f82e0e7ee36fac
mohamed7ali <mohamed77mosleh77@gmail.com>
2024-01-22 14:51:18 +0200

    malloc files

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,4 +1,4 @@
-i#ifndef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
 
 #include <stdio.h>
@@ -31,5 +31,4 @@ char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
-
 #endif
